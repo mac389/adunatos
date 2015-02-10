7b3acfebbb980f205711c4edfa4655ca38003491
Michael Chary <mac389@gmail.com>
2015-02-10 13:05:08 -0500

    updated main sh

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
-cs src
+cd src
 python differential-pathway-analysis
+echo 'Extracted regions of interest by z-score, deduplicated'
 python add-uniprot.py
+echo 'Found Uniprot alias for genes'
 python add-GO.py
+echo 'Mapped genes to gene ontology using Uniprot alias'
 python compare-expression.py
+echo 'Made figure comparing expression, printed list of upregulated genes to'
